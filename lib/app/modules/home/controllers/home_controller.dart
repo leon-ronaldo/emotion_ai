165afa4973c99fdfed8a14ebf3ed826f908cd845
leon ronaldo <ronaldoleon029@gmail.com>
2024-04-14 15:33:14 +0530

    messed things up

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,30 +1,35 @@
-import 'package:emotion_ai/modules/geminiInteraction-geminiAPI-module.dart';
-import 'package:emotion_ai/modules/textToSpeech-cloudTextToSpeech-module.dart';
+// ignore_for_file: empty_catches
+
+import 'package:emotion_ai/modules/conversationGenerator-gemini-module.dart';
+import 'package:emotion_ai/modules/voiceRecognition.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class HomeController extends GetxController {
   double screenHeight = 0, screenWidth = 0;
 
-  late GeminiInteraction geminiAPI;
-  late TextToSpeechEngine speechEngine;
   late TextEditingController requestFieldController;
+  late ConversationGenerator conversationGenerator;
 
   Rx<String> responseText = ''.obs;
   Rx<bool> screenReady = false.obs, fetchingResponse = false.obs;
 
   @override
-  void onInit() {
+  void onInit() async {
     super.onInit();
 
     screenHeight = Get.context!.height;
     screenWidth = Get.context!.width;
 
-    geminiAPI = GeminiInteraction();
-    speechEngine = TextToSpeechEngine();
+    conversationGenerator = ConversationGenerator();
     requestFieldController = TextEditingController();
+    await conversationGenerator.init();
+    conversationGenerator.speechRecognition.startListening();
 
-    screenReady.value = geminiAPI.geminiIsReady && speechEngine.speechToTextIsReady;
+    screenReady.value = conversationGenerator.isReady;
+
+    // conversationGenerator.speechRecognition.startListening();
+    // conversationGenerator.detectSpeechContext();
   }
 
   @override
@@ -35,5 +40,8 @@ class HomeController extends GetxController {
   @override
   void onClose() {
     super.onClose();
+    try {
+      conversationGenerator.speechRecognition.stopListening();
+    } on Exception catch (e) {}
   }
 }
--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -7,6 +7,9 @@ import '../controllers/home_controller.dart';
 
 class HomeView extends GetView<HomeController> {
   const HomeView({Key? key}) : super(key: key);
+
+  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,43 +31,47 @@ class HomeView extends GetView<HomeController> {
                                 SpinKitFadingCircle(color: Colors.blueAccent),
                           ),
                         ),
-                        Text('Getting Gemini And Speech Model Ready...'),
+                        Text('Getting All Modules Ready....'),
                       ],
                     )
-                  : Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Container(
-                          margin: const EdgeInsets.only(bottom: 20),
-                          width: controller.screenWidth * 0.6,
-                          child: TextField(
-                            controller: controller.requestFieldController,
-                            onSubmitted: (text) async {
-                              controller.responseText.value =
-                                  await controller.geminiAPI.getResponse(text);
+                  : Container(
+                      margin: const EdgeInsets.only(top: 50),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Container(
+                            margin: const EdgeInsets.only(bottom: 20),
+                            width: controller.screenWidth * 0.6,
+                            child: TextField(
+                              controller: controller.requestFieldController,
+                              onSubmitted: (text) async {
+                                controller.responseText.value = await controller
+                                    .conversationGenerator.geminiAPI
+                                    .getResponse(text);
 
-                              await controller.speechEngine
-                                  .speak(controller.responseText.value);
-                            },
-                            decoration: const InputDecoration(
-                                hintText: 'Enter question or prompt',
-                                border: OutlineInputBorder()),
+                                await controller.conversationGenerator.speechEngine
+                                    .speak(controller.responseText.value);
+                              },
+                              decoration: const InputDecoration(
+                                  hintText: 'Enter question or prompt',
+                                  border: OutlineInputBorder()),
+                            ),
                           ),
-                        ),
-                        controller.fetchingResponse.value
-                            ? Column(
-                                children: [
-                                  Container(
-                                      margin: const EdgeInsets.all(10),
-                                      width: 50,
-                                      height: 50,
-                                      child: const SpinKitFadingCircle(
-                                          color: Colors.blueAccent)),
-                                  const Text('Fetching response...'),
-                                ],
-                              )
-                            : Text(controller.responseText.value),
-                      ],
+                          controller.fetchingResponse.value
+                              ? Column(
+                                  children: [
+                                    Container(
+                                        margin: const EdgeInsets.all(10),
+                                        width: 50,
+                                        height: 50,
+                                        child: const SpinKitFadingCircle(
+                                            color: Colors.blueAccent)),
+                                    const Text('Fetching response...'),
+                                  ],
+                                )
+                              : Text(controller.conversationGenerator.speechRecognition.result.value),
+                        ],
+                      ),
                     ),
             )),
       ),
